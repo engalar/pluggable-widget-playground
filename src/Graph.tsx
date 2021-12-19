b121597a064e05377b10d565411530c168cef3ea
engalar <engalar@gmail.com>
2021-12-19 17:03:39 +0800

    第一课：展示实体属性

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,8 +1,17 @@
-import { createElement } from "react";
+import { createElement, useMemo } from "react";
+import { ValueStatus } from 'mendix';
 import { GraphContainerProps } from "../typings/GraphProps";
 
 export default function (props: GraphContainerProps) {
-    console.log(props);
+    const myAttribute1 = useMemo(() => {
+        if (props.myAttribute1?.status === ValueStatus.Available) {
+            return props.myAttribute1.value;
+        }
+    }, [props.myAttribute1]);
 
-    return <div></div>;
+    return <div>
+        <h1>自定义组件（第一课）</h1>
+        <h2><span className="alert-info panel">Default value</span>     {props.sampleText}</h2>
+        <h2><span className="alert-info panel">当前组件实体上下文的字符属性</span>     {myAttribute1 ? myAttribute1 : '--'}</h2>
+    </div>;
 }
