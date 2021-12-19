bebcb3b260723430beda30d483e3a8f613f476a1
engalar <engalar@gmail.com>
2021-12-19 13:30:39 +0800

    Initial commit

---
+++ src/Graph.tsx
@@ -0,0 +1,8 @@
+import { createElement } from "react";
+import { GraphContainerProps } from "../typings/GraphProps";
+
+export default function (props: GraphContainerProps) {
+    console.log(props);
+
+    return <div></div>;
+}
